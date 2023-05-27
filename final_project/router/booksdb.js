81fddbe0400665d03835fb2fefea96867df40010
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-27 14:30:10 +0100

    Adding ISBN to booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,64 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  1: {
+    "author": "Chinua Achebe",
+    "title": "Things Fall Apart",
+    "isbn": "978-0385474542",
+    "reviews": {},
+  },
+  2: {
+    "author": "Hans Christian Andersen",
+    "title": "Fairy tales",
+    "isbn": "978-1503275928",
+    "reviews": {},
+  },
+  3: {
+    "author": "Dante Alighieri",
+    "title": "The Divine Comedy",
+    "isbn": "978-1503275928",
+    "reviews": {},
+  },
+  4: {
+    "author": "Unknown",
+    "title": "The Epic Of Gilgamesh",
+    "isbn": "978-0140449198",
+    "reviews": {},
+  },
+  5: {
+    "author": "Unknown",
+    "title": "The Book Of Job",
+    "isbn": "978-0140448818",
+    "reviews": {},
+  },
+  6: {
+    "author": "Unknown",
+    "title": "One Thousand and One Nights",
+    "isbn": "978-0140442892",
+    "reviews": {},
+  },
+  7: {
+    "author": "Unknown",
+    "title": "Nj\u00e1l's Saga",
+    "isbn": "978-0140447699",
+    "reviews": {},
+  },
+  8: {
+    "author": "Jane Austen",
+    "title": "Pride and Prejudice",
+    "isbn": "978-0141439518",
+    "reviews": {},
+  },
+  9: {
+    "author": "Honor\u00e9 de Balzac",
+    "title": "Le P\u00e8re Goriot",
+    "isbn": "978-0140449723",
+    "reviews": {},
+  },
+  10: {
+    "author": "Samuel Beckett",
+    "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+    "isbn": "978-0802151361",
+    "reviews": {},
+  },
+};
 
-module.exports=books;
+module.exports = books;
