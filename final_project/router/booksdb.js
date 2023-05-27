05a2f32f32a7b29140be948e0b0e9a886c7e0f4e
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-28 00:00:06 +0100

    Removing sample reviews from bookdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -3,123 +3,61 @@ let books = {
     author: "Chinua Achebe",
     title: "Things Fall Apart",
     isbn: "978-0385474542",
-    reviews: {
-      review1: "Great book! Highly recommended.",
-      review2: "A masterpiece of African literature.",
-      review3: "The story and characters are captivating.",
-    },
+    reviews: {},
   },
   2: {
     author: "Hans Christian Andersen",
     title: "Fairy tales",
     isbn: "978-1503275928",
-    reviews: {
-      review1: "Classic tales that never get old.",
-      review2: "Wonderful collection of stories for all ages.",
-      review3: "Beautifully written with enchanting narratives.",
-    },
+    reviews: {},
   },
   3: {
     author: "Dante Alighieri",
     title: "The Divine Comedy",
     isbn: "978-1503275928",
-    reviews: {
-      review1:
-        "A literary masterpiece that explores the depths of hell, purgatory, and heaven.",
-      review2:
-        "Dante's imaginative journey through the afterlife is awe-inspiring.",
-      review3: "An epic poem that leaves a lasting impact on readers.",
-    },
+    reviews: {},
   },
   4: {
     author: "Unknown",
     title: "The Epic Of Gilgamesh",
     isbn: "978-0140449198",
-    reviews: {
-      review1:
-        "One of the oldest known works of literature with powerful themes.",
-      review2:
-        "The story of Gilgamesh and his quest for immortality is timeless.",
-      review3: "An ancient epic that still resonates with readers today.",
-    },
+    reviews: {},
   },
   5: {
     author: "Unknown",
     title: "The Book Of Job",
     isbn: "978-0140448818",
-    reviews: {
-      review1:
-        "A profound and thought-provoking exploration of human suffering.",
-      review2:
-        "The poetic language and philosophical discussions make this book a must-read.",
-      review3:
-        "An ancient text that raises timeless questions about the nature of faith and suffering.",
-    },
+    reviews: {},
   },
   6: {
     author: "Unknown",
     title: "One Thousand and One Nights",
     isbn: "978-0140442892",
-    reviews: {
-      review1:
-        "A collection of captivating stories that transport readers to a world of enchantment.",
-      review2:
-        "The tales within this book are a treasure trove of imagination and cultural richness.",
-      review3:
-        "Each story is beautifully woven, keeping readers engaged from start to finish.",
-    },
+    reviews: {},
   },
   7: {
     author: "Unknown",
     title: "Njál's Saga",
     isbn: "978-0140447699",
-    reviews: {
-      review1:
-        "An epic saga that immerses readers in the Viking Age and explores themes of honor and vengeance.",
-      review2:
-        "The characters and their complex relationships make this saga a compelling read.",
-      review3:
-        "A gripping tale of feuds and tragedies that provides a glimpse into medieval Iceland.",
-    },
+    reviews: {},
   },
   8: {
     author: "Jane Austen",
     title: "Pride and Prejudice",
     isbn: "978-0141439518",
-    reviews: {
-      review1:
-        "A timeless classic with witty dialogue and memorable characters.",
-      review2:
-        "Jane Austen's social commentary and storytelling make this novel a delight to read.",
-      review3:
-        "The romance and social dynamics in this book are beautifully depicted.",
-    },
+    reviews: {},
   },
   9: {
     author: "Honoré de Balzac",
     title: "Le Père Goriot",
     isbn: "978-0140449723",
-    reviews: {
-      review1:
-        "A tragic tale that delves into the complexities of human relationships and societal pressures.",
-      review2:
-        "Balzac's vivid descriptions and character development make this novel a masterpiece.",
-      review3:
-        "A compelling exploration of greed, love, and sacrifice set in early 19th-century Paris.",
-    },
+    reviews: {},
   },
   10: {
     author: "Samuel Beckett",
     title: "Molloy, Malone Dies, The Unnamable, the trilogy",
     isbn: "978-0802151361",
-    reviews: {
-      review1:
-        "A challenging yet rewarding trilogy that pushes the boundaries of language and existence.",
-      review2:
-        "Beckett's minimalist style and existential themes make this trilogy a unique reading experience.",
-      review3:
-        "An introspective journey into the depths of human consciousness and isolation.",
-    },
+    reviews: {},
   },
 };
 
