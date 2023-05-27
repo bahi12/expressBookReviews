25e47eb611c2fa758a682108ceb9ed4a3df2a062
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-27 16:33:55 +0100

    Getting book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,8 +60,15 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const book = Object.values(books).find((b) => b.isbn === isbn);
+
+  if (book) {
+    const reviews = book.reviews;
+    res.send(JSON.stringify(reviews));
+  } else {
+    res.send(`No book found for ISBN: ${isbn}`);
+  }
 });
 
 module.exports.general = public_users;
